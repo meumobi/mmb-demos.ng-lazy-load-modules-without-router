68677abcd75b6975be6ba2dd6838bffe2e82be5c
vdias38 <victor.dias@meumobi.com>
2022-10-11 11:43:24 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mmb-demos.ng-lazy-load-modules-without-router';
+}
